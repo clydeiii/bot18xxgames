8be668e8fcf5f484fbae1cd4ad29f63089927840
Wright <cwwrigh@evoforge.org>
2020-07-28 17:06:54 -0400

    make it better

---
+++ bot.js
@@ -0,0 +1,19 @@
+// load .env file
+require('dotenv').config();
+const Discord = require('discord.js');
+const client = new Discord.Client();
+//const auth = require('./auth.json');
+
+
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('pong');
+  }
+});
+
+client.login(process.env.DISCORD_TOKEN);
