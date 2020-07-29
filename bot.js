008276e1fb020fbab8b35339fa860df1a67f0985
Wright <cwwrigh@evoforge.org>
2020-07-29 12:59:47 -0400

    used eslint to cleanup code

--- bot.js
+++ bot.js
@@ -2,18 +2,16 @@
 require('dotenv').config();
 const Discord = require('discord.js');
 const client = new Discord.Client();
-
-var timeUp = Date();
-
+const timeUp = Date();
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+	console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('pong via @clydeiii#0966 (time up:'+timeUp+')');
-  }
+	if (msg.content === 'ping') {
+		msg.reply('pong via @clydeiii#0966 (time up:' + timeUp + ')');
+	}
 });
 
 client.login(process.env.DISCORD_TOKEN);
