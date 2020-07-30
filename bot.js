2239982c4d3b444e9dea9b3c7396e09bf916d95f
Wright <cwwrigh@evoforge.org>
2020-07-30 17:29:56 -0400

    made bot work

--- bot.js
+++ bot.js
@@ -1,17 +1,155 @@
-// load .env file
+// load .env file for heroku
 require('dotenv').config();
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const fetch = require('node-fetch');
 const timeUp = Date();
+const createCommand = '!create_game';
+const gameDatabase = new Discord.Collection();
+const internalPollingInterval = 10;
+const externalPollingInterval = 120;
+const webAPI = 'https://www.18xx.games/api/game/';
 
-client.on('ready', () => {
-	console.log(`Logged in as ${client.user.tag}!`);
-});
+class Game {
+	constructor(id, players, channel, guild) {
+		this._id = id;
+		this._players = players.array();
+		this._currentPlayer = players.first();
+		this._channel = channel;
+		this._guild = guild;
+		this._needsAlert = true;
+	}
+	set id(val) {
+		this._id = val;
+	}
+	get id() {
+		return this._id;
+	}
+	set players(allPlayers) {
+		this._players = allPlayers;
+	}
+	get players() {
+		return this._players;
+	}
+	set currentPlayer(currentPlayer) {
+		this._currentPlayer = currentPlayer;
+	}
+	get currentPlayer() {
+		return this._currentPlayer;
+	}
+	set needsAlert(alertValue) {
+		this._needsAlert = alertValue;
+	}
+	get needsAlert() {
+		return this._needsAlert;
+	}
+	set guild(serverName) {
+		this._guild = serverName;
+	}
+	get guild() {
+		return this._guild;
+	}
+	set channel(channel) {
+		this._channel = channel;
+	}
+	get channel() {
+		return this._channel;
+	}
 
+	advancePlayer(nextPlayerAccordingToWeb) {
+		// if the next player is different from the current player
+		let realNextUser = this._currentPlayer;
+
+		// we need to turn website's player name into discord player object here
+		// loop over all assigned discord users into this game and see if any match exists
+		for(const player of this._players) {
+			if(player.username === nextPlayerAccordingToWeb) {
+				realNextUser = player;
+			}
+		}
+		// if there's a new current player, we need to notify them by setting needsAlert to true
+		if(this._currentPlayer != realNextUser) {
+			// we need to alert the next player that it's their turn
+			console.log(`${nextPlayerAccordingToWeb}'s turn, time to notify them`);
+			this._needsAlert = true;
+		}
+		this._currentPlayer = realNextUser;
+	}
+
+	toString() {
+		return `game id ${this._id} with players ${this._players} in channel ${this._channel} on server ${this._guild} (current player is ${this._currentPlayer})`;
+	}
+}
+
+/** *****
+ *
+ * this is the "main" function
+ *
+ ****/
 client.on('message', msg => {
-	if (msg.content === 'ping') {
-		msg.reply('pong via @clydeiii#0966 (time up:' + timeUp + ')');
+	// console.log(msg.content);
+	if (msg.content.startsWith(createCommand)) {
+		const args = msg.content.slice(createCommand.length).trim().split(' ');
+		const gameID = args[0];
+		gameDatabase.set(gameID, new Game(gameID, msg.mentions.users, msg.channel, msg.guild));
+		msg.reply(`game created: ${gameDatabase.get(gameID).toString()}`);
+	}
+	if (msg.content === '!list_games') {
+		for(const gameID of gameDatabase.keys()) {
+			msg.reply(`${gameID} : ${gameDatabase.get(gameID).toString()}`);
+		}
+	}
+	if (msg.content === '!wwjcld') {
+		msg.reply('clearclaw would dump B&O on you right now');
 	}
 });
 
+// log that bot is up and online
+client.once('ready', () => {
+	console.log(`Logged in as ${client.user.tag} at uptime: ${timeUp}`);
+});
+
+/*
+ * async function getCurrentPlayersFromWeb queries 18xx.games website via web API, given an game ID, then uses that update our internal database
+ */
+const getCurrentPlayerFromWeb = async gameID => {
+	try {
+		console.log(`obtaining active player from ${webAPI}${gameID}`);
+		const response = await fetch(webAPI + gameID);
+		const json = await response.json();
+		json.players.forEach(player => {
+			if(player['id'] === parseInt(json.acting)) {
+				console.log(`active player ${player['name']}`);
+				const game = gameDatabase.get(gameID);
+				game.advancePlayer(player['name']);
+			}
+		});
+	}
+	catch (error) {
+		console.log(error);
+	}
+};
+
+// inject bot token in real time from .env variable
 client.login(process.env.DISCORD_TOKEN);
+
+// start a background process that runs every 10 seconds and alerts any players whose turn it is
+setInterval (function() {
+	for(const gameID of gameDatabase.keys()) {
+		const game = gameDatabase.get(gameID);
+		if (game.needsAlert) {
+			game.channel.send(`${game.currentPlayer} it is your turn in https://www.18xx.games/game/${gameID}`).catch(console.error);
+			game.needsAlert = false;
+		}
+	}
+}, internalPollingInterval * 1000);
+
+// start background process that queries 18xx.games every n seconds
+setInterval (function() {
+	// loop over all games we know about and query web for each one
+	for(const gameID of gameDatabase.keys()) {
+		const game = gameDatabase.get(gameID);
+		getCurrentPlayerFromWeb(game.id);
+	}
+}, externalPollingInterval * 1000);
+
